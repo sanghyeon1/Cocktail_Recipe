31c146a9eea5a2678d94a6f76a6d306c7754c074
sanghyeon1 <53289616+sanghyeon1@users.noreply.github.com>
2021-05-25 23:40:48 +0900

    add function "valid_search"
    add function "valid_search"

--- Cocktail_Recipe.py
+++ Cocktail_Recipe.py
@@ -1,6 +1,8 @@
 from cock_information import *
 
 all_cocktail = cocktails()
+# 만들 수 있는 칵테일의 정보를 담고, 원하는 칵테일의 레시피를 보여주기 위한 변수.
+valid_cocktail = []
 
 
 #  칵테일의 정보들을 출력해주는 함수
@@ -36,11 +38,29 @@ def search():
         print("찾는 칵테일이 없습니다.")
 
 
+def valid_search(name):
+    check = 0
+    print("{}의 레시피를 출력합니다.".format(name))
+    for i in all_cocktail:
+        if name == i[0]:
+            print("=====================================")
+            information(i)  # 칵테일 정보 출력
+            print("=====================================")
+        else:
+            check += 1
+            pass
+
+    if check == len(all_cocktail):
+        print("찾는 칵테일이 없습니다.")
+
+
 #  현재 내가 가지고 있는 재료를 가지고 뭘 만들 수 있는지 찾는 함수
 def available_to_make():
     inhand = ['peach liqueur', 'tequila', 'rum', 'gin', 'bodka',
               'triple sec', 'blue currasso', 'kahlua', 'midori', 'grenadine syrup',
-              'creme de cassis', 'salt/pepper']
+              'creme de cassis', 'salt/pepper', 'sugar', 'club soda']
+
+    global valid_cocktail
 
     for i in range(len(all_cocktail)):
         name = all_cocktail[i]
@@ -53,18 +73,34 @@ def available_to_make():
                 break
         if stack >= len(name[3]):
             print("{}은 가지신 재료로 만들 수 있습니다.".format(name[0]))
+            valid_cocktail.append(name[0])
 
 
 def main():
     while True:
-        work = input("무엇을 하시겠습니까?\n[1] 칵테일 레시피 검색\n[2] 가지고있는 재료로 만들 수 있는 칵테일 찾기\n--> ")
+        work = input("무엇을 하시겠습니까?\n[1] 칵테일 레시피 검색\n[2] 가지고있는 재료로 만들 수 있는 칵테일 찾기\n[x] 프로그램 종료\n--> ")
+        if work == 'x':
+            print("프로그램을 종료합니다.")
+            break
+
         if work == '1':
             search()
         elif work == '2':
             available_to_make()
-
+            print("==========================================================================")
+            print("칵테일 이름을 출력합니까?\n만들 수 있는 칵테일 목록에서 찾고싶은 칵테일의 순번을 입력해주십시오.")
+            print("취소는 x 입니다.")
+            while True:
+                show_recipe = input("--> ")
+                if show_recipe == 'x':
+                    break
+                elif 0 <= int(show_recipe) <= len(valid_cocktail)+1:
+                    valid_search(valid_cocktail[int(show_recipe) - 1])
+                    break
+                else:
+                    print("잘못된 값입니다, 다시 입력해주세요.", end='')
         else:
-            print("1 또는 2 를 입력해주세요.", end='')
+            print("잘못된 값입니다, 다시 입력해주세요.", end='')
         work = 0
         print("\n")
 
