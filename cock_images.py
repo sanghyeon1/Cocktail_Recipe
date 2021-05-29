3c07ba2cd163a86e9037b46a783cdc863e2776fe
sanghyeon1 <53289616+sanghyeon1@users.noreply.github.com>
2021-05-29 22:00:41 +0900

    Create new module cock_images
    Create new module cock_images

---
+++ cock_images.py
@@ -0,0 +1,27 @@
+def cocktail_image():
+    transparent = ['transparent', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/transparent_image.png']
+
+    kir = ['kir', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/kir.png']
+    negroni = ['negroni', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/negroni.png']
+    old_fashioned = ['old_fashioned', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/old_fashioned.png']
+    rusty_nail = ['rusty_nail', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/rusty_nail.png']
+    black_russian = ['black_russian', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/black_russian.png']
+    moscow_mule = ['moscow_mule', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/moscow_mule.png']
+    bloody_mary = ['bloody_mary', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/bloody_mary.png']
+    seabreeze = ['seabreeze', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/seabreeze.png']
+    cuba_libre = ['cuba_libre', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/cuba_libre.png']
+    long_island_ice_tea = ['long_island_ice_tea', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/long_island_iced_tea.png']
+    harvey_wallbanger = ['harvey_wallbanger', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/harvey_wallbanger.png']
+    tequila_sunrise = ['tequila_sunrise', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/tequila_sunrise.png']
+    mai_tai = ['mai_tai', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/mai_tai.png']
+    blue_hawaiian = ['blue_hawaiian', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/blue_hawaiian.png']
+    pina_colada = ['pina_colada', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/pina_colada.png']
+    b_52 = ['b_52', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/b_52.png']
+    pousse_cafe = ['pousse_cafe', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/pousse_cafe.png']
+    manhattan = ['manhattan', 'C:/Users/psh990429/PycharmProjects/cocktailcocktailError/images/manhattan.png']
+
+    all_images = [transparent, kir, negroni, old_fashioned, rusty_nail, black_russian, moscow_mule, bloody_mary, seabreeze, cuba_libre,
+                    long_island_ice_tea, harvey_wallbanger, tequila_sunrise, mai_tai, blue_hawaiian, pina_colada, b_52,
+                    pousse_cafe, manhattan]
+
+    return all_images
