d663e5ccaaf63f390fb8b362b65441a1ebf0283c
sanghyeon1 <53289616+sanghyeon1@users.noreply.github.com>
2021-06-04 11:06:23 +0900

    add some cocktail images
    add some cocktail images

--- cock_images.py
+++ cock_images.py
@@ -4,16 +4,9 @@ import pathlib
 def cocktail_image():
     # 이미지의 저장경로를 불러옴.
     image_path = str(pathlib.Path(__file__).parent.absolute())
-    print(image_path)
-    slash = "/"
-    for i in range(len(image_path)):
-        if image_path[i] == "\\":
-            image_path = image_path.replace("\\", "/", 10)
-        else:
-            pass
+    image_path = image_path.replace("\\", "/", 10)
 
-    transparent = ['transparent', image_path + '/images/transparent_image.png']
-    print(transparent)
+    transparent = ['transparent', image_path + '/images/transparent_image2.png']
     kir = ['kir', image_path + '/images/kir.png']
     negroni = ['negroni', image_path + '/images/negroni.png']
     old_fashioned = ['old_fashioned', image_path + '/images/old_fashioned.png']
@@ -32,9 +25,37 @@ def cocktail_image():
     b_52 = ['b_52', image_path + '/images/b_52.png']
     pousse_cafe = ['pousse_cafe', image_path + '/images/pousse_cafe.png']
     manhattan = ['manhattan', image_path + '/images/manhattan.png']
+    dry_martini = ['dry_martini', image_path + '/images/dry_martini.png']
+    gochang = ['gochang', image_path + '/images/gochang.png']
+    sloe_gin_fizz = ['sloe_gin_fizz', image_path + '/images/sloe_gin_fizz.png']
+    whisky_sour = ['whisky_sour', image_path + '/images/whisky_sour.png']
+    singapore_sling = ['singapore_sling', image_path + '/images/singapore_sling.png']
+    honeymoon = ['honeymoon', image_path + '/images/honeymoon.png']
+    sidecar = ['sidecar', image_path + '/images/sidecar.png']
+    brandy_alexander = ['brandy_alexander', image_path + 'brandy_alexander/images/.png']
+    newyork = ['newyork', image_path + '/images/newyork.png']
+    margarita = ['margarita', image_path + '/images/margarita.png']
+    apple_martini = ['apple_martini', image_path + '/images/apple_martini.png']
+    kiss_of_fire = ['kiss_of_fire', image_path + '/images/kiss_of_fire.png']
+    cosmopolitan = ['cosmopolitan', image_path + '/images/cosmopolitan.png']
+    bacardi = ['bacardi', image_path + '/images/bacardi.png']
+    daiquiri = ['daiquiri', image_path + '/images/daiquiri.png']
+    june_bug = ['june_bug', image_path + '/images/june_bug.png']
+    grasshopper = ['grasshopper', image_path + '/images/grasshopper.png']
+    apricot = ['apricot', image_path + '/images/apricot.png']
+    geumsan = ['geumsan', image_path + '/images/geumsan.png']
+    jindo = ['jindo', image_path + '/images/jindo.png']
+    puppy_love = ['puppy_love', image_path + '/images/puppy_love.png']
+    healing = ['healing', image_path + '/images/healing.png']
 
-    all_images = [transparent, kir, negroni, old_fashioned, rusty_nail, black_russian, moscow_mule, bloody_mary, seabreeze, cuba_libre,
-                    long_island_ice_tea, harvey_wallbanger, tequila_sunrise, mai_tai, blue_hawaiian, pina_colada, b_52,
-                    pousse_cafe, manhattan]
+    all_images = [transparent, kir, negroni, old_fashioned, rusty_nail, black_russian, moscow_mule, bloody_mary,
+                  seabreeze, cuba_libre, long_island_ice_tea, harvey_wallbanger, tequila_sunrise, mai_tai,
+                  blue_hawaiian, pina_colada, b_52, pousse_cafe, manhattan, dry_martini, gochang, sloe_gin_fizz,
+                  whisky_sour, singapore_sling, honeymoon, sidecar, brandy_alexander, newyork, margarita, apple_martini,
+                  kiss_of_fire, cosmopolitan, bacardi, daiquiri, june_bug, grasshopper, apricot, geumsan, jindo,
+                  puppy_love, healing]
 
     return all_images
+
+
+cocktail_image()
